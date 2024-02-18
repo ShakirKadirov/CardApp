92c19f25ba262029d7c84fdace4cda0452a93729
ShakirKadirov <shakirkadirov1@gmail.com>
2024-02-18 14:11:14 +0600

    Initial Commit

---
+++ CardApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardApp
+//
+//  Created by Shakir Kadirov on 18.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
